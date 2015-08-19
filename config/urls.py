579a8f62836e781887b391da8e8db3367e451c66
Sviatoslav Bulbakha <ssbb@Sviatoslavs-MacBook-Pro.local>
2015-08-19 16:35:39 +0500

    Add media routing for development server

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -13,4 +14,4 @@ if settings.DEBUG:
 
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
