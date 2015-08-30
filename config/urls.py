61d329015d7501558d21df1de4929b7a28b82a16
Sviatoslav Bulbakha <ssbb@Sviatoslavs-MacBook-Pro.local>
2015-08-30 14:26:13 +0500

    add static urlpatterns in dev mode

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -15,3 +16,4 @@ if settings.DEBUG:
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
