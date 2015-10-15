e1f87ad80d7bd569a66fbfa555d117a7ccfccd2a
Sviatoslav Bulbakha <mail@ssbb.me>
2015-10-16 02:52:54 +0500

    update settings

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,4 +1,23 @@
+import logging
+
 from .base import *  # noqa
 
+DEBUG = False
+TEMPLATE_DEBUG = False
 
-TEST_RUNNER = 'rainbowtests.test.runner.RainbowDiscoverRunner'
+TEST_RUNNER = 'rainbowtests.test.runner.RainbowDiscoverCoverageRunner'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test_db'
+    }
+}
+
+CELERY_ALWAYS_EAGER = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+BROKER_BACKEND = 'memory'
+
+EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
+
+logging.disable(logging.CRITICAL)
