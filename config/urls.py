4fc25752499a70cee55a7444ec3dfd66b4468cd3
Sviatoslav Bulbakha <mail@ssbb.me>
2015-12-03 02:32:20 +0500

    upgrade admin include

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import include, url
-from django.contrib import admin
 from django.conf import settings
+from django.conf.urls import include, url
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
 
 
@@ -16,4 +15,5 @@ if settings.DEBUG:
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     urlpatterns += staticfiles_urlpatterns()
