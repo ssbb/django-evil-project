dbf5f11aa7a4890c49adf0c12598ead7d78d12b0
Sviatoslav Bulbakha <mail@ssbb.me>
2016-02-03 08:24:45 +0500

    remove template debug package

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,17 +1,12 @@
 from .base import *  # noqa
 
-
 INSTALLED_APPS += (
     'django_extensions',
     'debug_toolbar',
-    'template_debug',
 )
 
 MIDDLEWARE_CLASSES = (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ) + MIDDLEWARE_CLASSES
-
-# https://github.com/calebsmith/django-template-debug
-TEMPLATE_DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
